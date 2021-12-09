8a822650346d61dc1f233d3556d192639e50ad18
Ivan Ivanchenko <ivanisovich@yandex.ru>
2021-12-09 16:36:18 +0300

    remove authorization with the aim of deploying

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import "antd/dist/antd.css";
 import { Redirect, Route, Switch } from "react-router-dom";
 import PageConteiner from "./components/Page/PageContainer";
-// !import LoginConteiner from "./components/Login/LoginContainer";
+import LoginConteiner from "./components/Login/LoginContainer";
 
 const App = () => {
   return (
@@ -11,7 +11,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" render={() => <Redirect to={"/page"} />} />
         <Route path="/page" render={() => <PageConteiner />} />
-        {/* <Route path="/login" render={() => <LoginConteiner />} /> */}
+        <Route path="/login" render={() => <LoginConteiner />} />
       </Switch>
     </div>
   );
